04bebd238ac52391a76d4b7e307ee74db618b6b4
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-09-04 10:28:38 -0700

    1

---
+++ Card Holder/ViewController3.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController3.swift
+//  Card Holder
+//
+//  Created by Zulwiyoza Putra on 8/13/16.
+//  Copyright © 2016 Zulwiyoza Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController, UITableViewDelegate {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cardCell")
+        cell.textLabel!.text = "yay its working"
+        
+        return cell
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
